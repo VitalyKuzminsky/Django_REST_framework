0465f209d74b61468fe2f93f7bb612c35c252c21
Vitaly <info@myflyup.ru>
2023-01-05 16:35:18 +0300

    add requirements

--- librari/authapp/views.py
+++ librari/authapp/views.py
@@ -6,4 +6,3 @@ from .serializers import UserModelSerializer
 class UserModelViewSet(ModelViewSet):
     queryset = User.objects.all()  # Мы работаем с моделью Author, со всеми её записями.
     serializer_class = UserModelSerializer  # Указываем сереализатор.
-
