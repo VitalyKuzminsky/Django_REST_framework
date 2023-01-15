98a56dae33cdd78456423b7451908d2da88ba955
Vitaly <info@myflyup.ru>
2023-01-15 21:19:03 +0300

    finished lesson 3

--- librari/app/serializers.py
+++ librari/app/serializers.py
@@ -1,10 +1,37 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
-from .models import Author
+from rest_framework.serializers import ModelSerializer, StringRelatedField, HyperlinkedModelSerializer
+from .models import Author, Biography, Book, Article
 
 
 class AuthorModelSerializer(HyperlinkedModelSerializer):
+# class AuthorModelSerializer(ModelSerializer):
     class Meta:
         model = Author
         fields = '__all__'  # передавать всё
-        # fields = ('first_name',)  # передавать только указанные поля
+        # fields = ['first_name']  # передавать только указанные поля
+        # exclude = ['id']  # передавать всё, кроме этого
 
+
+class BiographyModelSerializer(HyperlinkedModelSerializer):
+# class BiographyModelSerializer(ModelSerializer):
+    class Meta:
+        model = Biography
+        fields = '__all__'
+
+
+class BookModelSerializer(HyperlinkedModelSerializer):
+# class BookModelSerializer(ModelSerializer):
+#     author = StringRelatedField(many=True)  # Указываем в книгах сериализатор для авторов, чтобы вместо id видеть имена
+    author = AuthorModelSerializer(many=True)
+
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+
+class ArticleModelSerializer(HyperlinkedModelSerializer):
+# class ArticleModelSerializer(ModelSerializer):
+    author = AuthorModelSerializer()  # Указываем в книгах сериализатор для автора, чтобы вместо id видеть имя
+
+    class Meta:
+        model = Article
+        fields = '__all__'
